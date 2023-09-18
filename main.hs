f9f7dc405acc108f8b1d9bfcd8643776b608fa7f
DavidEsdrs <carpina1983@gmail.com>
2023-09-18 15:01:26 -0300

    first commit

---
+++ main.hs
@@ -0,0 +1,29 @@
+import Data.Char (digitToInt)
+
+doublen x = x * 2
+
+doubleus x y = doublen x + doublen y
+
+doublebign x = if x > 100 then x else doublen x
+
+squared n = n * n
+
+circleArea r = pi * squared r
+
+squareArea s = squared s
+
+isPrime n | n <= 1    = False
+          | otherwise = all (\x -> n `mod` x /= 0) [2..sqrt_n]
+          where sqrt_n = floor (sqrt (fromIntegral n))
+
+primes = filter isPrime [2..]
+
+stringToNumberList :: String -> [Int]
+stringToNumberList str = map digitToInt str
+
+sumList :: [Int] -> Int
+sumList l = foldl (\acc x -> acc + x) 0 l
+
+digitsSum :: Int -> Int
+digitsSum n = sumList nums 
+    where nums = stringToNumberList (show n)
